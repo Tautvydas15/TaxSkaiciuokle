bd558eb8e85548eae42ee5c8adfb91e23ebb6234
TAUTVYDAS\tautv <tautvydastraigis@gmail.com>
2022-09-09 14:33:31 +0300

    Add project files.

---
+++ LastWork/Test/TestFullName.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastWork.Test
+{
+    internal class TestFullName
+    {
+        [Test]
+        public static void TestName()
+        {
+            string name = "Tautvydas";
+
+            IWebDriver driver = new ChromeDriver
+            {
+                Url = "https://www.tax.lt/login"
+            };
+
+            IWebElement fullNameInputField = driver.FindElement(By.Id("user_login"));
+            fullNameInputField.SendKeys(name);
+
+            IWebElement submitButton = driver.FindElement(By.CssSelector("#submit"));
+            submitButton.Click();
+
+            IWebElement result = driver.FindElement(By.Id("name"));
+            Assert.AreEqual($"Name:{name}", result.Text, "Name is wrong!");
+
+            driver.Close();
+        }
+
+    }
+}
+
+
---
+++ LastWork/Test/WebTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Webtest
+
+{
+    class TestWebDriver
+    {
+        [Test]
+        public static void TestChromeDriver()
+        {
+            IWebDriver driver = new ChromeDriver
+            {
+                Url = "https://www.tax.lt/"
+            };
+            Thread.Sleep(2000);
+            driver.Close();
+        }
+
+        [Test]
+        public static void TestFirefoxDriver()
+        {
+            IWebDriver driver = new FirefoxDriver
+            {
+                Url = "https://www.tax.lt/"
+            };
+            Thread.Sleep(2000);
+            driver.Close();
+        }
+
+
+    }
+}
+
